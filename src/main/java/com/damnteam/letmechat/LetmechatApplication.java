8a84a63bc69471fd77a55b31664c094d8c876e76
Mikhail Viktarenkau <vikt@epolsoft.com>
2019-11-18 18:41:10 +0300

    init commit

---
+++ src/main/java/com/damnteam/letmechat/LetmechatApplication.java
@@ -0,0 +1,13 @@
+package com.damnteam.letmechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetmechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damnteam/letmechat/LetmechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damnteam.letmechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetmechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
