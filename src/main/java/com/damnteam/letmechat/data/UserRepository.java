3d3483fb1fe14d023d093be62328f5b15543d3a3
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2019-11-20 01:13:05 +0300

    Update index page, user repo init

--- src/main/java/com/damnteam/letmechat/data/UserRepository.java
+++ src/main/java/com/damnteam/letmechat/data/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByLastName(String lastName);
+    List<User> findByLogin(String login);
 
     User findById(long id);
 }
