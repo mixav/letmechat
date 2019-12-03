f852ad4528e78e660221b7d14fcec992caae2c19
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2019-12-03 19:39:32 +0300

    Add: GenericMessage for messaging

---
+++ src/main/java/com/damnteam/letmechat/data/GenericMessage.java
@@ -0,0 +1,17 @@
+package com.damnteam.letmechat.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GenericMessage {
+
+    String username;
+
+    String message;
+}
