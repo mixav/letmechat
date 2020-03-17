c7e2a24b99606c5e6009f29a1df35b9eac138891
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2020-03-17 19:01:45 +0300

    todo

---
+++ frontend-letmechat/src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-icon-circle :style="{color:online?green:red}"></b-icon-circle>
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+export default {
+  name: "User",
+  props: {
+    name: String,
+    id: Number,
+    online: Boolean
+  }
+};
+</script>
